429f6ca79795f5a68b8b1770712e675d800c49e8
jijogeorgepde-eng <jijogeorge.pde@gmail.com>
2026-09-23 07:57:14 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "location" {
+    type = string
+    default = "eastus"
+    description = "azure default region"
+}                                                                                          
+
+variable "rg" {
+    type = string
+    default = "kml_rg_main-8d75c55334be4e4d"
+    description = "my resource group"
+}
