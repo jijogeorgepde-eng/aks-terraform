d9b529c3d05db2f97b3aa2d8611aa941c42d7729
jijogeorgepde-eng <jijogeorge.pde@gmail.com>
2026-09-23 22:31:47 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,6 +6,6 @@ variable "location" {
 
 variable "rg" {
     type = string
-    default = "kml_rg_main-8d75c55334be4e4d"
+    default = "kml_rg_main-b9522bd63b85426c"
     description = "my resource group"
 }
